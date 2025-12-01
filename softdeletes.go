f486a1afbd4ece13141fe4683dd4ef8fa112a228
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-11-30 21:05:56 -0700

    feat: set object type to ensure updates are not soft deleted for dependent edges
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- softdeletes.go
+++ softdeletes.go
@@ -20,11 +20,19 @@ func CheckSkipSoftDelete(ctx context.Context) bool {
 type SoftDeleteKey struct{}
 
 // IsSoftDelete returns a new context that informs the delete is a soft-delete for interceptor/hooks.
-func IsSoftDelete(parent context.Context) context.Context {
-	return context.WithValue(parent, SoftDeleteKey{}, true)
+func IsSoftDelete(parent context.Context, objectType string) context.Context {
+	return context.WithValue(parent, SoftDeleteKey{}, objectType)
+}
+
+// CheckIsSoftDeleteType checks if the softDeleteKey is set in the context for the given object type
+func CheckIsSoftDeleteType(ctx context.Context, objectType string) bool {
+	val, ok := ctx.Value(SoftDeleteKey{}).(string)
+
+	return ok && val == objectType
 }
 
 // CheckIsSoftDelete checks if the softDeleteKey is set in the context
+// @deprecated use CheckIsSoftDeleteType with object type instead
 func CheckIsSoftDelete(ctx context.Context) bool {
 	return ctx.Value(SoftDeleteKey{}) != nil
 }
