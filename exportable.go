3bae3413a960b356d97b0c53482ee510c0d082f9
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-01-24 12:36:23 -0700

    feat: exports filters (#231)
    * feat: ability to add owner and system owned fields automatically to query
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * feat: add system owned and owner id filters automatically
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- exportable.go
+++ exportable.go
@@ -13,7 +13,42 @@ import "encoding/json"
 //			entx.Exportable{},
 //		}
 //	}
-type Exportable struct{}
+type Exportable struct {
+	// OrgOwned indicates if the schema is owned by an organization and will automatically add the ownerID filter during export validation.
+	OrgOwned bool
+	// HasSystemOwned indicates if the schema has systemOwned field and will automatically add the systemOwned=false filter during export validation.
+	HasSystemOwned bool
+}
+
+// Options for the Exportable annotation.
+type ExportableOption func(*Exportable)
+
+// NewExportable creates a new Exportable annotation with the given options.
+func NewExportable(opts ...ExportableOption) Exportable {
+	e := Exportable{}
+
+	for _, opt := range opts {
+		opt(&e)
+	}
+
+	return e
+}
+
+// WithOrgOwned is an option for the Exportable annotation
+// that indicates the schema is owned by an organization.
+func WithOrgOwned() ExportableOption {
+	return func(e *Exportable) {
+		e.OrgOwned = true
+	}
+}
+
+// WithSystemOwned is an option for the Exportable annotation
+// that indicates the schema is owned by the system.
+func WithSystemOwned() ExportableOption {
+	return func(e *Exportable) {
+		e.HasSystemOwned = true
+	}
+}
 
 // Name returns the name of the Exportable annotation.
 func (Exportable) Name() string {
